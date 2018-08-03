67970022aef43e076dd3da9553f60258e31c0b96
vishwakalyanaraman <vishwa.kalyanaraman@gmail.com>
2018-08-03 23:37:45 +0530

    Change wordcloud generation details

--- gen_wordcloud.py
+++ gen_wordcloud.py
@@ -9,10 +9,6 @@ text = open(path.join(d, 'CleanedComments.txt')).read()
 # Generate a word cloud image
 wordcloud = WordCloud().generate(text)
 
-# Display the generated image:
-plt.imshow(wordcloud, interpolation='kaiser')
-plt.axis("off")
-
 #Second image with a lower font size
 wordcloud = WordCloud(max_font_size=40,max_words=2000,normalize_plurals=True).generate(text)
 plt.figure()
